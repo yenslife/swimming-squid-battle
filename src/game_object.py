6097d34b9be5333278d6ebc929b60f645f65dfde
Kylin_on_Mac <kylingithubdev@gmail.com>
2022-08-03 15:08:36 +0800

    update ai and game_object.py

--- src/game_object.py
+++ src/game_object.py
@@ -2,11 +2,20 @@ import random
 
 import pygame.sprite
 
+from mlgame.view.view_model import create_rect_view_data
+
+BALL_VEL = 10.5
+
+BALL_H = 100
+
+BALL_W = 10
+
 
 class Ball(pygame.sprite.Sprite):
     def __init__(self, color="#FFEB3B"):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([50, 50])
+        self.origin_image = pygame.Surface([BALL_W, BALL_H])
+        self.image = self.origin_image
         self.color = color
         self.rect = self.image.get_rect()
         self.rect.center = (400, 300)
@@ -14,25 +23,31 @@ class Ball(pygame.sprite.Sprite):
     def update(self, motion):
         # for motion in motions:
         if motion == "UP":
-            self.rect.centery -= 10.5
+            self.rect.centery -= BALL_VEL
         elif motion == "DOWN":
-            self.rect.centery += 10.5
+            self.rect.centery += BALL_VEL
         elif motion == "LEFT":
-            self.rect.centerx -= 10.5
+            self.rect.centerx -= BALL_VEL
+            # self.angle += 5
         elif motion == "RIGHT":
-            self.rect.centerx += 10.5
+            self.rect.centerx += BALL_VEL
+            # self.angle -= 5
+        # self.image = pygame.transform.rotate(self.origin_image, self.angle)
+        # print(self.angle)
+        # center = self.rect.center
+        # self.rect = self.image.get_rect()
+        # self.rect.center = center
 
     @property
     def game_object_data(self):
-        return {"type": "rect",
-                "name": "ball",
-                "x": self.rect.x,
-                "y": self.rect.y,
-                "angle": 0,
-                "width": self.rect.width,
-                "height": self.rect.height,
-                "color": self.color
-                }
+        return create_rect_view_data(
+            "ball",
+            self.rect.x,
+            self.rect.y,
+            self.rect.width,
+            self.rect.height,
+            self.color
+        )
 
 
 class Food(pygame.sprite.Sprite):
